d8dd0396b2cf1cff0c6000dbf0bc515a9e408ff8
unknown <chinubaladash19@gmail.com>
2024-02-05 11:08:07 +0530

    footer section change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -56,7 +56,13 @@ export const Footer = () => {
 						</div>
 					</div>
 				</div>
-			</section>
+				<hr style={{height:"2px",borderWidth:"0",color:"gray",backgroundColor:"gray"}}/>
+				<div  className="text-center ">
+				<h6 className="text-white display-5">2024 ALL RIGHTS RESERVED | POWERED BY <span className="text-warning">PROPERTY VISTA</span></h6>
+				<h6>DISCLAMIER</h6>
+				</div>
+				
+							</section>
 		</>
 	);
 };
